9a1f13c44eb632c021db3f6de22d63ab3ccb7f13
Sakib Rahman <seu.sakib@gmail.com>
2022-02-13 15:51:02 +0600

    add commenting

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/* --------------------
+    common functions 
+--------------------- */
+
 function updateProductNum(isIncreasing, elementID) {
   const quantityText = document.getElementById(elementID);
   const quantity = parseInt(quantityText.value);
@@ -15,6 +19,10 @@ function productTotalPrice(elementID, quantity, unitPrice) {
   document.getElementById(elementID).innerText = quantity * unitPrice;
 }
 
+/* ----------------------------------------
+   handal phone increas and decreas event 
+----------------------------------------- */
+
 // phone plus button
 document.getElementById("phone-plus").addEventListener("click", function () {
   const quantity = updateProductNum(true, "phone-quantity");
@@ -25,6 +33,11 @@ document.getElementById("phone-minus").addEventListener("click", function () {
   const quantity = updateProductNum(false, "phone-quantity");
   productTotalPrice("phone-total-price", quantity, 1219);
 });
+
+/* ----------------------------------------
+   handal case increas and decreas event 
+----------------------------------------- */
+
 // case plus button
 document.getElementById("case-plus").addEventListener("click", function () {
   const quantity = updateProductNum(true, "case-quantity");
