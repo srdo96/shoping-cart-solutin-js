119dd63db69c17a2155f9614c465c1d363796dd0
Sakib Rahman <seu.sakib@gmail.com>
2022-02-13 14:33:39 +0600

    add a function to calculate case total price.

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ function updateCase(isIncreasing) {
   const quantityText = document.getElementById("case-quantity");
   const quantity = parseInt(quantityText.value);
   if (isIncreasing) {
-    quantityText.value = quantity + 1;
+    return (quantityText.value = quantity + 1);
   } else if (quantity > 0) {
-    quantityText.value = quantity - 1;
+    return (quantityText.value = quantity - 1);
   }
+}
+function caseTotalPrice(quantity) {
+  document.getElementById("case-total-price").innerText = quantity * 59;
 }
 
 // case plus button
 document.getElementById("case-plus").addEventListener("click", function () {
-  updateCase(true);
+  const quantity = updateCase(true);
+  caseTotalPrice(quantity);
 });
 
 // case minus button
 document.getElementById("case-minus").addEventListener("click", function () {
-  updateCase(false);
+  const quantity = updateCase(false);
+  caseTotalPrice(quantity);
 });
